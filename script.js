df772922c8bd9e70b0dcd8c7d78a39f893bba9e5
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-30 20:54:49 -0700

    adding frontend files

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const searchNameBTNClick = async () => {
+    console.log('searchNameBTNClick')
+    const query = {
+        type: "nameSearch", 
+        name: $("#searchName").val(),
+        projectionFilters: {
+            name: true,
+            weight: false
+        }
+    }
+
+    const res = await axios.post('https://bright-toga-bat.cyclic.app/search', query)
+    $("#searchResults").empty()
+    $("#searchResults").html(JSON.stringify(res.data))
+
+
+}
+
+const searchWeightBTNClick = () => {
+    console.log('searchWeightBTNClick')
+}
+
+const searchFoodBTNClick = () => {
+    console.log('searchFoodBTNClick')
+}
+
+const filtersProjection = () => {
+    console.log('filtersProjection clicked')
+}
+
+const setup = () => {
+    console.log("Setup")
+
+    $("#searchNameBTN").click(searchNameBTNClick)
+    $("#searchWeightBTN").click(searchWeightBTNClick)
+    $("#searchFoodBTN").click(searchFoodBTNClick)
+    $("#filtersProjection").click(filtersProjection)
+}
+
+$("document").ready(setup)
