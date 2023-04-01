20ca93b265a2e40283d2fff4c5a2fe61534b178e
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-31 19:56:04 -0700

    implemented search and sort logic

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 
 const searchNameBTNClick = async () => {
     console.log('searchNameBTNClick')
+    var nameFilter = $("#nameFilter").prop("checked")
+    var weightFilter = $("#weightFilter").prop("checked")
     const query = {
         type: "nameSearch", 
         name: $("#searchName").val(),
         projectionFilters: {
-            name: true,
-            weight: false
+            name: nameFilter,
+            weight: weightFilter
         }
     }
 
     const res = await axios.post('https://kind-pike-hospital-gown.cyclic.app/search', query)
+    // const res = await axios.post('http://localhost:3000/search', query)
     $("#searchResults").empty()
     $("#searchResults").html(JSON.stringify(res.data))
 
 
 }
 
-const searchWeightBTNClick = () => {
+const searchWeightBTNClick = async () => {
     console.log('searchWeightBTNClick')
+    var nameFilter = $("#nameFilter").prop("checked")
+    var weightFilter = $("#weightFilter").prop("checked")
+    var upper_bound = parseInt($("#maxweight").val())
+    var lower_bound = parseInt($("#minweight").val())
+    const query = {
+        type: "weightSearch", 
+        weight_upper: upper_bound,
+        weight_lower: lower_bound,
+        projectionFilters: {
+            name: nameFilter,
+            weight: weightFilter
+        }
+    }
+
+    const res = await axios.post('https://kind-pike-hospital-gown.cyclic.app/search', query)
+    // const res = await axios.post('http://localhost:3000/search', query)
+    $("#searchResults").empty()
+    $("#searchResults").html(JSON.stringify(res.data))
+
+
 }
 
-const searchFoodBTNClick = () => {
+
+const searchFoodBTNClick = async () => {
     console.log('searchFoodBTNClick')
+    var nameFilter = $("#nameFilter").prop("checked")
+    var weightFilter = $("#weightFilter").prop("checked")
+    var apple = $("#foodInput1").prop("checked")
+    var carrot = $("#foodInput2").prop("checked")
+    var loves_list = []
+    console.log(apple)
+    console.log(carrot)
+    if (apple) {
+        loves_list.push("apple")
+    }
+    if (carrot) {
+        loves_list.push("carrot")
+    }
+    const query = {
+        type: "foodSearch", 
+        loves: loves_list,
+        projectionFilters: {
+            name: nameFilter,
+            weight: weightFilter
+        }
+    }
+
+    const res = await axios.post('https://kind-pike-hospital-gown.cyclic.app/search', query)
+    // const res = await axios.post('http://localhost:3000/search', query)
+    $("#searchResults").empty()
+    $("#searchResults").html(JSON.stringify(res.data))
+
+
 }
 
 const filtersProjection = () => {
